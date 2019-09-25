1bee934658d7b75485df6f7c23c8457dc1fed6f9
24 <462501908@qq.com>
2019-09-25 21:11:42 +0800

    修改了绝对路径和相对路径

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Button } from 'antd';
 
+import Test from 'component/Test';
+
 import styles from './index.module.scss';
 
 const Home = () => (
   <div className={ styles.container }>
     <div className={ styles.content}>
     <Button type="primary">Primary</Button>
-        欢迎来到德莱联盟
+        <Test />
     </div>
   </div>
 );
