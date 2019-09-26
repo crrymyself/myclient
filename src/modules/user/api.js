5ddd3c3f88ebf79eacf779b05065c92786504540
24 <462501908@qq.com>
2019-09-26 13:38:53 +0800

    修改打包错误

--- src/modules/user/api.js
+++ src/modules/user/api.js
@@ -1,10 +1,10 @@
 import Axios from 'axios';
 
-export const  postApiLogin = payload => 
+export const  postApiLogin = payload =>
   new Promise((resolve, reject) => {
     Axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/login`, {
       ...payload,
     })
       .then(resolve)
-      .catch(reject)
-  })
+      .catch(reject);
+  });
