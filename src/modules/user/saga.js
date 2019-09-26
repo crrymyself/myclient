370af618adcbdae100ff2681a81b1c60db582e51
24 <462501908@qq.com>
2019-09-26 14:05:27 +0800

    调试

--- src/modules/user/saga.js
+++ src/modules/user/saga.js
@@ -1,22 +1,23 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+
 import { postApiLogin } from './api';
 import { userFail } from './action';
 
-import { call, put, takeLatest } from 'redux-saga/effects';
 
 function* watchPostLogin() {
   yield takeLatest('POST_LOGIN_START', onPostLogin);
 }
 
-function* onPostLogin({ payload, cb}) {
+function* onPostLogin({ payload, cb }) {
   try {
     const { data } = yield call(postApiLogin, payload);
     if (data.token) {
       window.localStorage.setItem('user', data.token);
-      cb && cb()
+      cb && cb();
     }
-  } catch(error) {
+  } catch (error) {
     if (error.response) {
-      console.log(error.response);
+      // console.log(error.response);
     }
     yield onError(error);
   }
