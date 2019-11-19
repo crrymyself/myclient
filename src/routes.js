32fc6591c71d9c3c126e30fb3c41cfaf21045a0d
24 <462501908@qq.com>
2019-11-19 23:11:18 +0800

    引入react-loadble优化router加载

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,59 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Redirect, Route, Router, Switch } from 'react-router-dom';
+import {  Redirect, Route, Router, Switch } from 'react-router-dom';
+// import { ConnectedRouter } from 'connected-react-router';
 // import pathToRegexp from 'path-to-regexp';
+import Loadable from 'react-loadable';
 
-import Articles from './Pages/Articles';
-import AboutUs from './Pages/AboutUs';
 import BasicLayout from 'layouts/BasicLayout';
 
+const loadingPage = ({ error, pastDelay }) => {
+  if (error) {
+    return (
+      <div>Error!</div>
+    );
+  } else if (pastDelay) {
+    return <div />;
+  }
+  return null;
+};
+
+const routeConfig = [
+  {
+    name: 'articles',
+    path: '/articles',
+    exact: true,
+    component: Loadable({
+      loader: () => import('./Pages/Articles'),
+      loading: loadingPage,
+      delay: 200,
+    }),
+  },
+  {
+    name: 'about',
+    path: '/about',
+    exact: true,
+    component: Loadable({
+      loader: () => import('./Pages/AboutUs'),
+      loading: loadingPage,
+      delay: 200,
+    }),
+  },
+];
 const Routes = ({ history }) => (
   <Router history={ history }>
     <Switch>
       <BasicLayout>
-        <Route component={ Articles } exact path='/articles' />
-        <Route component={ AboutUs } exact path='/about' />
+        {
+          routeConfig.map((r, key)=> (
+            <Route
+              component={ r.component }
+              exact={ !!r.exact }
+              key={ key }
+              path={ r.path } />
+          ))
+        }
         <Redirect exact from='/' to='/articles' />
       </BasicLayout>
     </Switch>
